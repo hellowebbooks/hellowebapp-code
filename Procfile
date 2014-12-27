3767982b98c8188e8aa5942f4aa9f7c5d8660e41
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 19:06:42 -0800

    End of Chapter 13

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT hellowebapp.wsgi:application
--- hellowebapp/wsgi.py
+++ hellowebapp/wsgi.py
@@ -1,14 +1,9 @@
-"""
-WSGI config for hellowebapp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
-"""
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellowebapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+    "hellowebapp.settings_production")
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
