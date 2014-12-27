b7a5fc46aa0ddc8a18414f8e5420d9cbdb0554c5
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 17:12:17 -0800

    End of Chapter 5

--- collection/views.py
+++ collection/views.py
@@ -9,8 +9,10 @@ from collection.models import Thing
 
 def index(request):
     number = 6
+
     # don't forget the quotes because it's a string, not an integer
     thing = "Thing name"
+
     return render(request, 'index.html', {
         'number': number,
         # don't forget to pass it in, and the last comma
