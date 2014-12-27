84e4b7b0cea348118fe0e58a62839b93896310e4
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 17:28:22 -0800

    End of Chapter 9

--- collection/views.py
+++ collection/views.py
@@ -100,6 +100,7 @@ def browse_by_name(request, initial=None):
         'initial': initial,
     })
 
+
 def thing_detail(request, slug):
     # grab the object...
     thing = Thing.objects.get(slug=slug)
@@ -107,4 +108,32 @@ def thing_detail(request, slug):
     # and pass to the template
     return render(request, 'things/thing_detail.html', {
         'thing': thing,
+    })
+
+
+def edit_thing(request, slug):
+    # grab the object...
+    thing = Thing.objects.get(slug=slug)
+
+    # set the form we're using...
+    form_class = ThingForm
+
+    # if we're coming to this view from a submitted form,
+    if request.method == 'POST':
+        # grab the data from the submitted form
+        form = form_class(data=request.POST, instance=thing)
+
+        if form.is_valid():
+            # save the new data
+            form.save()
+            return redirect('thing_detail', slug=thing.slug)
+
+    # otherwise just create the form
+    else:
+        form = form_class(instance=thing)
+
+    # and render the template
+    return render(request, 'things/edit_thing.html', {
+        'thing': thing,
+        'form': form,
     })
