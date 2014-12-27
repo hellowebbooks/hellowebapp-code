ba4e138fb6c3c998653d105491506a5027f91728
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 19:06:42 -0800

    End of Chapter 13

--- hellowebapp/settings.py
+++ hellowebapp/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 ACCOUNT_ACTIVATION_DAYS = 7
 
---
+++ hellowebapp/settings_production.py
@@ -0,0 +1,19 @@
+# Inherit from standard settings file for defaults
+from hellowebapp.settings import *
+
+# Everything below will override our standard settings:
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Set debug to False
+DEBUG = False 
+
+# Static asset configuration
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
