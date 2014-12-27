d20cd1d7fae0d46dfa101ed10ca64628e95b21e1
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 17:15:45 -0800

    End of Chapter 6

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+# import your model
+from collection.models import Thing
+
+# set up automated slug creation
+class ThingAdmin(admin.ModelAdmin):
+    model = Thing
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+# and register it
+admin.site.register(Thing, ThingAdmin)
