aefaeaa643f6d85119f89d64dc5b3bf72159fadc
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 17:25:05 -0800

    End of Chapter 8

--- collection/views.py
+++ collection/views.py
@@ -86,3 +86,12 @@ def create_thing(request):
     return render(request, 'things/create_thing.html', {
         'form': form,
     })
+
+def thing_detail(request, slug):
+    # grab the object...
+    thing = Thing.objects.get(slug=slug)
+
+    # and pass to the template
+    return render(request, 'things/thing_detail.html', {
+        'thing': thing,
+    })
