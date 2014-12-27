2cb2d5cd3eaf3c9f97725fcdf702a1eca006c150
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 17:28:22 -0800

    End of Chapter 9

--- collection/views.py
+++ collection/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from collection.forms import ThingForm
 from collection.models import Thing
 
 
@@ -10,6 +11,7 @@ def index(request):
         'things': things,
     })
 
+
 def thing_detail(request, slug):
     # grab the object...
     thing = Thing.objects.get(slug=slug)
@@ -17,4 +19,32 @@ def thing_detail(request, slug):
     # and pass to the template
     return render(request, 'things/thing_detail.html', {
         'thing': thing,
+    })
+
+
+def edit_thing(request, slug):
+    # grab the object...
+    thing = Thing.objects.get(slug=slug)
+
+    # set the form we're using...
+    form_class = ThingForm
+
+    # if we're coming to this view from a submitted form,  
+    if request.method == 'POST':
+        # grab the data from the submitted form
+        form = form_class(data=request.POST, instance=thing)
+
+        if form.is_valid():
+            # save the new data
+            form.save()
+            return redirect('thing_detail', slug=thing.slug)
+
+    # otherwise just create the form
+    else:
+        form = form_class(instance=thing)
+
+    # and render the template
+    return render(request, 'things/edit_thing.html', {
+        'thing': thing,
+        'form': form,
     })
