9155924271d40ba9ed430c3c7645b0b64d639d08
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 17:25:05 -0800

    End of Chapter 8

--- collection/views.py
+++ collection/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+
 from collection.models import Thing
+
 
 # the rewritten view!
 def index(request):
     things = Thing.objects.all()
     return render(request, 'index.html', {
         'things': things,
+    })
+
+def thing_detail(request, slug):
+    # grab the object...
+    thing = Thing.objects.get(slug=slug)
+
+    # and pass to the template
+    return render(request, 'things/thing_detail.html', {
+        'thing': thing,
     })
