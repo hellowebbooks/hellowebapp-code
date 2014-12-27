7b4897d49c544e3f84d611914c639f2210afa973
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 18:13:42 -0800

    End of Chapter 11

--- collection/views.py
+++ collection/views.py
@@ -160,3 +160,16 @@ def create_thing(request):
     return render(request, 'things/create_thing.html', {
         'form': form,
     })
+
+
+def browse_by_name(request, initial=None):
+    if initial:
+        things = Thing.objects.filter(
+             name__istartswith=initial).order_by('name')
+    else:
+        things = Thing.objects.all().order_by('name')
+
+    return render(request, 'search/search.html', {
+        'things': things,
+        'initial': initial,
+    })
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import path
 from django.contrib import admin
 from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 from collection.backends import MyRegistrationView
 from collection import views
@@ -14,8 +14,15 @@ urlpatterns = [
     path('contact/',
         TemplateView.as_view(template_name='contact.html'), name='contact'),
 
+    path('things/', RedirectView.as_view(pattern_name='browse')),
     path('things/<slug>/', views.thing_detail, name='thing_detail'),
     path('things/<slug>/edit/', views.edit_thing, name='edit_thing'),
+
+    path('browse/', RedirectView.as_view(pattern_name='browse')),
+    path('browse/name/',
+        views.browse_by_name, name='browse'),
+    path('browse/name/<initial>/', 
+        views.browse_by_name, name='browse_by_name'),
 
     path('accounts/password/reset/', password_reset,
         {'template_name': 'registration/password_reset_form.html'}, name="password_reset"),
@@ -29,7 +36,7 @@ urlpatterns = [
 
     path('accounts/register/',
         MyRegistrationView.as_view(), name='registration_register'),
-    path('accounts/create_thing/', 
+    path('accounts/create_thing/',
         views.create_thing, name='registration_create_thing'),
 
     path('accounts/', include('registration.backends.simple.urls')),
