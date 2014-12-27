67ff11250a8b0752e1ef2f0493c1948295d07cbf
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 17:15:45 -0800

    End of Chapter 6

--- collection/models.py
+++ collection/models.py
@@ -2,4 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Thing(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
