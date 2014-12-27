dbe75106ba2b36f99c374d638f796a178ba54ef5
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 18:20:51 -0800

    End of Chapter 12

--- collection/tests.py
+++ collection/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class CollectionTest(TestCase):
+    def test_index(self):
+        r = self.client.get('/')
+        self.assertEqual(r.status_code, 200)
+
+    def test_no_logic_page(self):
+        r = self.client.get('/about/')
+        self.assertEqual(r.status_code, 200) 
