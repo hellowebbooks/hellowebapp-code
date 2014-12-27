35e4e306783fef11849d0516d5de12a5bd8e9e28
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 17:02:37 -0800

    End of Chapter 4

--- collection/views.py
+++ collection/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
-# Create your views here.
+
+def index(request):
+   # this is your new view
+   return render_to_response('index.html')
