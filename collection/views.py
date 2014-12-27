7d6559e76a4dd61b31e6bba4a401d1d3d2c1fd4c
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 17:12:17 -0800

    End of Chapter 5
    Updated for Hello Web App v1.1 and Django 1.9

--- collection/views.py
+++ collection/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 
 def index(request):
-   # this is your new view
-   return render(request, 'index.html')
+    number = 6
+    # don't forget the quotes because it's a string, not an integer
+    thing = "Thing name"
+    return render(request, 'index.html', {
+        'number': number,
+        # don't forget to pass it in, and the last comma
+        'thing': thing,
+    })
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from collection import views
 
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
+    url(r'^about/$',
+        TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^contact/$',
+        TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^admin/', include(admin.site.urls)),
 ]
