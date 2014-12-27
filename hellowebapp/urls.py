df67bc19a11820acf03a1d87e2a799d13e1a896c
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 17:12:17 -0800

    End of Chapter 5
    Updated for Hello Web App v1.1 and Django 1.9

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import path
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from collection import views
 
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/',
+        TemplateView.as_view(template_name='about.html'), name='about'),
+    path('contact/',
+        TemplateView.as_view(template_name='contact.html'), name='contact'),
     path('admin/', admin.site.urls),
 ]
