fa5d8f62e4280257e084ccd547431919171a6136
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 17:19:34 -0800

    End of Chapter 7

--- collection/views.py
+++ collection/views.py
@@ -6,17 +6,13 @@ from django.template.defaultfilters import slugify
 from collection.forms import ThingForm
 from collection.models import Thing
 
-
+# the rewritten view!
 def index(request):
-    number = 6
 
-    # don't forget the quotes because it's a string, not an integer
-    thing = "Thing name"
+    things = Thing.objects.all()
 
     return render(request, 'index.html', {
-        'number': number,
-        # don't forget to pass it in, and the last comma
-        'thing': thing,
+        'things': things,
     })
 
 
