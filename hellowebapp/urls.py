7d45fd000c0ffd03c29c872112954459c5109c92
Tracy Osborn <tracy@weddinglovely.com>
2018-07-20 13:39:13 -0600

    Updated for Django 2.0

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url, include
+from django.conf.urls import path
 from django.contrib import admin
 
 from collection import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='home'),
-    url(r'^admin/', include(admin.site.urls)),
+    path('', views.index, name='home'),
+    path('admin/', admin.site.urls),
 ]
